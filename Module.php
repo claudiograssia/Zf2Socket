faccb881febe5602dc7cab1adc4fdb6a173b368a
Claudio <claudiograssia0000@gmail.com>
2015-06-18 23:18:05 +0200

    Update Module.php

--- Module.php
+++ Module.php
@@ -9,5 +9,15 @@ class Module {
       'SocketServer' => 'Zf2Socket\Service\Server'
     ];
   }
+  
+  public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+  }
 
 }
