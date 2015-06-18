a99b2fcb8877e87c2ebecb9ce863a9ba8361443b
Claudio <claudiograssia0000@gmail.com>
2015-06-18 23:25:36 +0200

    Update Server.php

--- src/Zf2Socket/Service/Server.php
+++ src/Zf2Socket/Service/Server.php
@@ -5,14 +5,27 @@ namespace Zf2Socket\Service;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocator;
 
+use Hoa\Socket\Server as ServerSocket;
+
 class Server implements FactoryInterface {
 
   public function createService(ServiceLocator $serviceLocator) {
     return $this;
   }
   
-  public function init() {
-  
+  public function init(
+      $socket,
+      Closure $closure
+  ) {
+     $server = new ServerSocket($socket);
+     $server->connectAndWait();
+     
+     while(true) {
+          foreach ($server->select() as $node) {
+              $clouse->__invoke($server); 
+          }
+     }
+     
   }
   
 }
