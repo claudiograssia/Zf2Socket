764a27f5627b16f112a8460a2a952d59bd7aaae0
Claudio <claudiograssia0000@gmail.com>
2015-07-27 17:43:05 +0200

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,19 +1,19 @@
 <?php 
 return [
   'console' => [
-        'router' => [
-            'routes' => [
-                'indexer' => [
-                    'options' => [
-                        'route' => 'socket test',
-                        'defaults' => [
-                            'controller' => 'Zf2SocketTestController',
-                            'action' => 'index'
-                        ]
-                    ]
-                ]
-            ],
-        ],
+    'router' => [
+      'routes' => [
+        'indexer' => [
+          'options' => [
+            'route' => 'socket test',
+            'defaults' => [
+              'controller' => 'Zf2SocketTestController',
+              'action' => 'index'
+            ]
+          ]
+        ]
+      ],
+    ],
   ],
   'controllers' => [
     'invokables' => [
